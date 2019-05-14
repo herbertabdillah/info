adf6bcfd11027baef75e6e7b4421b5e8001b695c
herbertabdillah <abdillah.herbert@gmail.com>
2019-05-14 10:22:30 +0700

    init

---
+++ precache-manifest.3866c3d47991a47f7cb500ae31f9c118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5ea8010cf41c6453687",
+    "url": "/static/js/main.f5ea8010.chunk.js"
+  },
+  {
+    "revision": "94b577d01eb43b22e574",
+    "url": "/static/js/1.94b577d0.chunk.js"
+  },
+  {
+    "revision": "f5ea8010cf41c6453687",
+    "url": "/static/css/main.c3f6b302.chunk.css"
+  },
+  {
+    "revision": "6006cf1dbcd3a6d1f31ea589069d6f7b",
+    "url": "/index.html"
+  }
+];
